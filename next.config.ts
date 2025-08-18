a15e0cce7ecbdb3b8ab7e226dddf3dac1a79112c
Colton Miller <coltonmiller2@gmail.com>
2025-08-18 18:06:06 +0000

    fix: Enable static export for Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,7 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // This is the line you need to add
+  output: 'export',
 };
 
 export default nextConfig;
