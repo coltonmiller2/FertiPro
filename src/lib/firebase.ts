f4a710155688cc427bf8bce804ed2f6311f93bd7
Colton Miller <coltonmiller2@gmail.com>
2025-08-21 21:09:39 +0000

    wait a minute - I tihnk I may have discovered something. I am not in the

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,18 +6,18 @@ import { getFirestore } from "firebase/firestore"; // Import Firestore
 // This is a public configuration and is safe to expose.
 const firebaseConfig = {
   apiKey: "AIzaSyD_k5b7Q-E8z1m_e9xP-jZ_k6c9Y0f4w3A",
-  authDomain: "backyard-bounty-5v5mw.firebaseapp.com",
-  projectId: "backyard-bounty-5v5mw",
-  storageBucket: "backyard-bounty-5v5mw.appspot.com",
-  messagingSenderId: "172564531486",
-  appId: "1:172564531486:web:8325d37e40ee08a39a210f"
+  authDomain: "project-controls-hub.firebaseapp.com",
+  projectId: "project-controls-hub",
+  storageBucket: "project-controls-hub.appspot.com",
+  messagingSenderId: "351226791458",
+  appId: "1:351226791458:web:f108b4822a971a430eac7b"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Get Firestore instance for the specific database
-const db = getFirestore(app, "backyard-bounty-5v5mw");
+// Get Firestore instance
+const db = getFirestore(app);
 
 // Export Firestore instance
 export { db };
