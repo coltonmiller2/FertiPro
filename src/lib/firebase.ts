8b2a286ca2cd3103a3a7830a9fef87b40358bda3
Colton Miller <coltonmiller2@gmail.com>
2025-08-21 20:54:16 +0000

    see the below warnings from the web console: [2025-08-21T20:53:21.771Z]

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -17,7 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Get Firestore instance
+// Get Firestore instance for the specific database
 const db = getFirestore(app);
 
 // Export Firestore instance
