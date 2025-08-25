5a185b0ce44cd50026004599af0e4cda8ea22b25
Colton Miller <coltonmiller2@gmail.com>
2025-08-25 17:10:02 +0000

    When I deploy this app to my site and move the symbols around and add/de

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,11 +6,11 @@ import { getFirestore } from "firebase/firestore"; // Import Firestore
 // This is a public configuration and is safe to expose.
 const firebaseConfig = {
   apiKey: "AIzaSyD_k5b7Q-E8z1m_e9xP-jZ_k6c9Y0f4w3A",
-  authDomain: "project-controls-hub.firebaseapp.com",
-  projectId: "project-controls-hub",
-  storageBucket: "project-controls-hub.appspot.com",
-  messagingSenderId: "351226791458",
-  appId: "1:351226791458:web:f108b4822a971a430eac7b"
+  authDomain: "backyard-bounty-5v5mw.firebaseapp.com",
+  projectId: "backyard-bounty-5v5mw",
+  storageBucket: "backyard-bounty-5v5mw.appspot.com",
+  messagingSenderId: "172564531486",
+  appId: "1:172564531486:web:8325d37e40ee08a39a210f"
 };
 
 // Initialize Firebase
